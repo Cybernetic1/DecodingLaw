017d9a671f272079dfd895121ff30c4acd0bf72c
yky <generic.intelligence@gmail.com>
2018-04-01 21:07:20 +0800

    generate random words

--- gen-random-words.py
+++ gen-random-words.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 """
+0. for each category:
+1. read all cases in folder
+2.	  for each case generate n examples (select k random words)
+
 @author: YKY
 """
 import numpy as np
@@ -21,11 +25,6 @@ categories = ["matrimonial-rights", "separation", "divorce", "after-divorce", "d
 suffix = ""		# to be added to sub-directory, not needed currently
 
 # =================== Read case examples from file ======================
-"""
-0. for each category:
-1. read all cases in folder
-2.	  for each case generate n examples (select k random words)
-"""
 labels = []
 data = []
 fixed_seq_len = times_steps
@@ -33,97 +32,21 @@ fixed_seq_len = times_steps
 print("\n**** Reading pre-recorded message texts....")
 for i, category in enumerate(categories):
 	print("\nCategory: ", category)
-	for j, filename in enumerate(os.listdir("../scraped-data/" + category + suffix)):
+	for j, filename in enumerate(os.listdir("scraped-data/" + category + suffix)):
 		stuff = []
-		with open("../scraped-data/" + category + suffix + "/" + filename) as f:
+		with open("scraped-data/" + category + suffix + "/" + filename) as f:
 			for line in f:
 				line = re.sub(r'[^\w\s-]',' ',line)	# remove punctuations except hyphen
 				for word in line.lower().split():	# convert to lowercase
 					if word not in stopwords.words('english'):	# remove stop words
-						stuff.append(word)
-		print("Case-law #", j, " word count = ", len(stuff), '\n')
+						if word[0] not in "0123456789-":
+							stuff.append(word)
+		print("word count = ", len(stuff), '\n')
 
-		for n in range(0, 5):			# number of examples per file (default: 500)
-			for k in range(0, 5):		# number of words per example (default: 10)
+		for n in range(0, 3):			# number of examples per file (default: 500)
+			print(n + 1, end = ": ")
+			for k in range(0, 8):		# number of words per example (default: 10)
 				# Randomly select a words (of fixed length) in stuff text
 				pos = np.random.choice(range(0, len(stuff) - 1))
 				print(stuff[pos], end = ' AND ')
-			print()
-
-exit(0)
-
-# convert to 1-hot encoding for labels
-for i in range(len(labels)):
-	label = labels[i]
-	one_hot_encoding = [0] * num_classes
-	one_hot_encoding[label] = 1
-	labels[i] = one_hot_encoding
-
-num_examples = len(data)
-print("\nData size = ", num_examples, " examples")
-
-# ================ Find unique words ================
-
-print("\n**** Finding unique words....")
-word_list = []				# to store the list of words appearing in case-text
-index = 0
-for sent in data:
-	for word in sent.split():
-		if word not in word_list:
-			# make sure no Chinese chars or numerals inside word
-			if re.search(u'[\u4e00-\u9fff]+', word) == None and \
-					re.search(r'\d', word) == None:
-				word_list.append(word)
-				index += 1
-				print(word, "                        ", end='\r')
-				sys.stdout.flush()
-
-print(len(word_list), " unique words found")
-
-# ============== Create word-to-vector dictionary ===========
-
-print("\n**** Looking up word vectors....")
-word2vec_map = {}
-count_all_words = 0
-entry_number = 0
-glove_file = open(path_to_glove, "r")
-f2 = open("found-words.txt", "w")
-try:
-	for word_entry in glove_file:
-		vals = word_entry.split()
-		word = str(vals[0])
-		entry_number += 1
-		if word in word_list:
-			print(count_all_words, word, file=f2)
-			print(entry_number, count_all_words, word, "             ", end='\r')
-			sys.stdout.flush()
-			count_all_words += 1
-			coefs = np.asarray(vals[1:], dtype='float32')
-			coefs /= np.linalg.norm(coefs)
-			word2vec_map[word] = coefs
-		if count_all_words == len(word_list) - 1:
-			print("*** found all words ***")
-			break
-# if it takes too long to look up the entire dictionary, we can break it short
-except KeyboardInterrupt:
-	pass
-glove_file.close()
-f2.close()
-
-print("Vocabulary size = ", len(word2vec_map))
-
-pickling_on = open("training-data-3.pickle", "wb+")
-pickle.dump(data, pickling_on)
-pickling_on.close()
-
-pickling_on = open("training-labels-3.pickle", "wb+")
-pickle.dump(labels, pickling_on)
-pickling_on.close()
-
-pickling_on = open("training-word-list-3.pickle", "wb+")
-pickle.dump(word_list, pickling_on)
-pickling_on.close()
-
-pickling_on = open("training-word2vec-map-3.pickle", "wb+")
-pickle.dump(word2vec_map, pickling_on)
-pickling_on.close()
+			print("\b\b\b\b     ")			# erase the last AND
