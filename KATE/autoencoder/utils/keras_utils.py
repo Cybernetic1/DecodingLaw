3c845a9b34a214249275efb869bc920a6d37c507
yky <generic.intelligence@gmail.com>
2018-04-07 00:10:50 +0800

    modified KATE autoencoder/utils/keras_utils.py and Keras-NN-training2.py

--- KATE/autoencoder/utils/keras_utils.py
+++ KATE/autoencoder/utils/keras_utils.py
@@ -113,18 +113,18 @@ class KCompetitive(Layer):
         P = (x + tf.abs(x)) / 2
         N = (x - tf.abs(x)) / 2
 
-        values, indices = tf.nn.top_k(P, topk / 2) # indices will be [[0, 1], [2, 1]], values will be [[6., 2.], [5., 4.]]
+        values, indices = tf.nn.top_k(P, topk // 2) # indices will be [[0, 1], [2, 1]], values will be [[6., 2.], [5., 4.]]
         # We need to create full indices like [[0, 0], [0, 1], [1, 2], [1, 1]]
         my_range = tf.expand_dims(tf.range(0, tf.shape(indices)[0]), 1)  # will be [[0], [1]]
-        my_range_repeated = tf.tile(my_range, [1, topk / 2])  # will be [[0, 0], [1, 1]]
+        my_range_repeated = tf.tile(my_range, [1, topk // 2])  # will be [[0, 0], [1, 1]]
         full_indices = tf.stack([my_range_repeated, indices], axis=2) # change shapes to [N, k, 1] and [N, k, 1], to concatenate into [N, k, 2]
         full_indices = tf.reshape(full_indices, [-1, 2])
         P_reset = tf.sparse_to_dense(full_indices, tf.shape(x), tf.reshape(values, [-1]), default_value=0., validate_indices=False)
 
 
-        values2, indices2 = tf.nn.top_k(-N, topk - topk / 2)
+        values2, indices2 = tf.nn.top_k(-N, topk - topk // 2)
         my_range = tf.expand_dims(tf.range(0, tf.shape(indices2)[0]), 1)
-        my_range_repeated = tf.tile(my_range, [1, topk - topk / 2])
+        my_range_repeated = tf.tile(my_range, [1, topk - topk // 2])
         full_indices2 = tf.stack([my_range_repeated, indices2], axis=2)
         full_indices2 = tf.reshape(full_indices2, [-1, 2])
         N_reset = tf.sparse_to_dense(full_indices2, tf.shape(x), tf.reshape(values2, [-1]), default_value=0., validate_indices=False)
