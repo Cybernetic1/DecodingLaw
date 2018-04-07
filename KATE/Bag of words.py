6afd25dc9d55bf20264a301f84e8aa9ae547ea4e
AbeerArora123 <u3534366@connect.hku.hk>
2018-04-07 23:28:37 +0800

    Input representation(still debugging)

---
+++ KATE/Bag of words.py
@@ -0,0 +1,57 @@
+# Feature extraction from text
+# Method: bag of words 
+# https://pythonprogramminglanguage.com
+ 
+from sklearn.feature_extraction.text import CountVectorizer
+import os
+import re
+from nltk.corpus import stopwords
+import numpy as np
+import pickle
+from math import sqrt
+
+def euclidean_distance(v):
+    return sqrt(sum(x**2 for x in v))
+
+fixed_seq_len = 32
+
+pickle_off = open("word2vec-map.pickle", "rb")
+word2vec_map = pickle.load(pickle_off)
+
+categories = ["matrimonial-rights", "separation", "divorce", "after-divorce", "divorce-maintenance",
+  "property-on-divorce", "types-of-marriages", "battered-wife-and-children", "Harmony-House", "divorce-mediation"]
+
+cats = []              # list of list of list of words (categories[lines[words]])
+suffix = ""            # to be added to sub-directory, not needed currently
+
+for i, category in enumerate(categories):
+    print("\nCategory: ", category)
+    for j, filename in enumerate(os.listdir("categories/" + category + suffix)):
+        with open("categories/" + category + suffix + "/" + filename) as f:
+            catLines = []
+            for line in f:
+                catWords = []
+                line = re.sub(r"[^\w-]", " ", line)             # strip punctuations except hyphen
+                line = re.sub(u"[\u4e00-\u9fff]", " ", line)    # strip Chinese
+                line = re.sub(r"\d", " ", line)                 # strip numbers
+                line = re.sub(r"-+", "-", line)                 # reduce multiple --- to -
+                for word in line.lower().split():
+                    if word not in stopwords.words('english'):
+                        catWords.append(word)
+                if len(catWords) > 0:                       # skip empty lines
+                    catLines.append(catWords)
+        cats.append(catLines)
+
+vec = np.empty(shape = (0,0))
+for i,category in enumerate(categories):
+	for line in cats[i]:
+		for word in line:
+			#print(word)
+			print(word2vec_map[word].shape)
+			#vec = np.concatenate(vec,np.asarray(word2vec_map[word]))
+
+sorted(vec, key=euclidean_distance)        
+
+
+file = open("prepared-data/train.pickle",'wb+')
+pickle.dump(vec, file)
